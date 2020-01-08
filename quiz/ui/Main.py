63a465e62415735c10c0092e266c6f7cca225e32
Lukas Loidolt <lukas.loidolt@chello.at>
2020-01-08 01:25:46 +0100

    initial commit

---
+++ quiz/ui/Main.py
@@ -0,0 +1,8 @@
+# This file is supposed to contain the main game loop and call necessary methods from other files
+
+# TODO: print introduction
+
+while True:
+    pass
+    # TODO: Ask question
+    # TODO: try printing answers
